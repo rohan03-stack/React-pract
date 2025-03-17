96b3e14f0d10cb4c891ee3f3028ec8eb635d8fb8
rohan03-stack <ronygaikwad03@gmail.com>
2025-03-18 02:36:15 +0530

    portfolio is added

--- portfolio-react/src/Home/Log.jsx
+++ portfolio-react/src/Home/Log.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import './logos.css'
 
 export const Log = () => {
   return (
     <div>
-        <h1>Companies I Have Worked For</h1>
-        <div>
-            <img src="https://storage.googleapis.com/gd-prod/images/a910d418-7123-4bc4-aa3b-ef7e25e74ae6.60c498c559810aa0.webp
-            " alt="" />
+        <h1 className='h2'>Companies I Have Worked For</h1>
+        <div className='underline'>
+    
+        </div>
+        <div className='lg'>
+            <img src="https://storage.googleapis.com/gd-prod/images/a910d418-7123-4bc4-aa3b-ef7e25e74ae6.60c498c559810aa0.webp" alt="" />
             <img src="https://i.pinimg.com/736x/54/2d/e3/542de3c5cde96f9df22afb07d0bec5ea.jpg" alt="" />
             <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="" />
+            <img src="https://1000logos.net/wp-content/uploads/2017/05/Paypal-Logo-2014.png" alt="" />
+            <img src="https://brandlogos.net/wp-content/uploads/2020/03/Netflix-logo.png" alt="" />
         </div>
     </div>
   )
