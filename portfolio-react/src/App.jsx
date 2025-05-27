21735adb9664ea44dd391121359402dc16c6feeb
rohan03-stack <ronygaikwad03@gmail.com>
2025-05-27 17:48:37 +0530

    portfolio hero file is added

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Navbar } from './Nav/Navbar'
-import { Homes } from './Home/Homes'
+import {React, useEffect} from 'react';
+import Aos from 'aos';
 
+// import 'aos/dist/aos.css'
 
 function App() {
-  
+  useEffect(() => {
+    Aos.init({
+      duration:1000,
+    })
+  },[])
 
   return (
     <>
+    
+<main className='bg-black'>
+ 
 
-    <Navbar />
-    <Homes />
-   
+</main>
+  
     </>
   )
 }
---
+++ portfolio-react/src/Components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className='relative overflow-hidden min-h-[550px] sm:min-h-[600px] flex flex-col items-center'>
+     <div className='md:h-[550px] h-[500px] w-[450px] bg-gradient-to-r absolute from-[#6d2897]' ></div>
+    </div>
+  )
+}
