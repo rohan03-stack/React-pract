e9be58b27baaa71eb272ea2143f7057ce7e516e2
rohan03-stack <ronygaikwad03@gmail.com>
2025-06-05 00:29:39 +0530

    about us file is added

--- portfolio-react/src/Components/Skills.jsx
+++ portfolio-react/src/Components/Skills.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import vue from '../assets/image-portfolio-master/vue.png'
+import react from '../assets/image-portfolio-master/icon_react.png'
+import html from '../assets/image-portfolio-master/html.png'
+import css from '../assets/image-portfolio-master/css-logo.png'
+import java from '../assets/image-portfolio-master/JavaScript-Logo.png'
+import query from '../assets/image-portfolio-master/jquery.png'
+import node from '../assets/image-portfolio-master/tailwindcss.png'
+import tailwind from '../assets/image-portfolio-master/node.png'
 
+const skillsData = [
+  {id:1,image:html, description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, distinctio?'},
+  {id:2,image:css, description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, distinctio?'},
+  {id:3,image:html, description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, distinctio?'},
+  {id:4,image:html, description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, distinctio?'},
+  {id:5,image:html, description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, distinctio?'},
+  {id:6,image:html, description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, distinctio?'},
+  {id:7,image:html, description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, distinctio?'}
+]
 export default function Skills() {
   return (
     <div>
-      
+        <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, distinctio?
+
+        </div>
     </div>
   )
 }
